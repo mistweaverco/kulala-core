6320f79103da685bed72025eb99486f719f64717
Marco Kellershoff <marco@mistweaverco.com>
2026-06-01 22:44:57 +0200

    fix(ci): tests in CI use dots reporter now

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -7,9 +7,4 @@ if [ -z "$CI" ]; then
   exit $?
 fi
 
-# Strip the first line (bun test version) and
-# strip empty newlines and invisible characters for cleaner logs in CI.
-
-export FORCE_COLOR=1;
-
-bun test --only-failures --randomize 2>&1 | tr -d '\r' | sed -r "s/\x1B\[[0-9;]*[KkJjGgHhA-DF-F]//g" | sed '/^[[:space:]]*$/d'
+bun test --only-failures --randomize --dots
