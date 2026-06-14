692a8ed03967f13ce8f60787b749a63cdb51c078
Marco Kellershoff <marco@mistweaverco.com>
2026-06-14 06:58:56 +0200

    fix(ci): publish npm script

--- scripts/publish-npm.sh
+++ scripts/publish-npm.sh
@@ -8,9 +8,6 @@ if [ -z "${VERSION:-}" ]; then
   exit 1
 fi
 
-ROOT="$(cd "$(dirname "$0")/.." && pwd)"
-cd "$ROOT"
-
 bun install --frozen-lockfile
 
 npm --prefix "${WRAPPER_DIR}" version "${VERSION}" --no-git-tag-version
@@ -21,7 +18,7 @@ if [ "${RESET_VERSIONS:-false}" = "true" ]; then
   npm version "0.0.0" --no-git-tag-version
 fi
 
-cd "../core-npm-wrapper"
+cd "${WRAPPER_DIR}"
 rm -rf dist/*
 bun run build
 npm publish --access public
