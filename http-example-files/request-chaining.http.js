f60868476c4f0446cb76d53709553a3d16469c08
Marco Kellershoff <marco@mistweaverco.com>
2026-06-26 14:51:33 +0200

    chore(http-example-files): restructure request chaining examples

--- http-example-files/request-chaining.http.js
+++ http-example-files/request-chaining.http.js
@@ -1,13 +1,13 @@
 const loginRes = await $kulala.runRequest(
   "login",
-  "./request-chaining-login.http",
+  "./request-chaining/request-chaining-login.http",
 );
 const token = loginRes.body.json.token;
 client.global.set("request-chaining-token", token);
 
 const userRes = await $kulala.runRequest(
   "get-users",
-  "./request-chaining-get-users.http",
+  "./request-chaining/request-chaining-get-users.http",
 );
 const users = userRes.body.json.data;
 
